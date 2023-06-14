932a184b6cba119000d96d372556ad410d7cd2d7
acalcutt <acalcutt@techidiots.net>
2023-06-14 01:38:46 -0400

    add download and terrainrgb script

---
+++ create_bathymetry_terrainrgb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+INPUT_DIR=./gebco
+OUTPUT_DIR=./output
+vrtfile=${OUTPUT_DIR}/gebco_terrainrgb0-9.vrt
+mbtiles=${OUTPUT_DIR}/gebco_terrainrgb0-9.mbtiles
+vrtfile2=${OUTPUT_DIR}/gebco_terrainrgb0-9_warp.vrt
+
+[ -d "$OUTPUT_DIR" ] || mkdir -p $OUTPUT_DIR || { echo "error: $OUTPUT_DIR " 1>&2; exit 1; }
+
+#rm rio/*
+gdalbuildvrt -overwrite ${vrtfile} ${INPUT_DIR}/*.tif
+gdalwarp -r cubic -s_srs epsg:4326 -t_srs EPSG:3857 -dstnodata 0 ${vrtfile} ${vrtfile2}
+rio rgbify -b -10000 -i 0.1 --min-z 0 --max-z 9 -j 24 --format png ${vrtfile2} ${mbtiles}
+
+sqlite3 ${mbtiles} 'CREATE UNIQUE INDEX tile_index on tiles (zoom_level, tile_column, tile_row);'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "gebco_terrainrgb_0-12" WHERE name = "name" AND value = "";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "GEBCO (2023) converted with rio rgbify" WHERE name = "description";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "png" WHERE name = "format";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "1" WHERE name = "version";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "baselayer" WHERE name = "type";'
+sqlite3 ${mbtiles} 'INSERT INTO metadata (name,value) VALUES(''attribution'',''GEBCO (2023)'');'
+sqlite3 ${mbtiles} 'PRAGMA journal_mode=DELETE;'
+
+
---
+++ download_extract.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+INPUT_DIR=./gebco
+[ -d "$INPUT_DIR" ] || mkdir -p $INPUT_DIR || { echo "error: $INPUT_DIR " 1>&2; exit 1; }
+cd $INPUT_DIR
+wget -O gebco_2023_sub_ice_topo_geotiff.zip https://www.bodc.ac.uk/data/open_download/gebco/gebco_2023_sub_ice_topo/geotiff/
+unzip gebco_2023_sub_ice_topo_geotiff.zip
+cd ..
+
