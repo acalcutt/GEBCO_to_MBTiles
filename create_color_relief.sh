3f6b273fe9933cd42e119c69b8c33d23c5d13c12
acalcutt <acalcutt@techidiots.net>
2023-06-15 22:03:42 -0400

    add color relief script and color ramps

---
+++ create_color_relief.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+
+INPUT_DIR=./gebco
+OUTPUT_DIR=./output
+vrtfile=${OUTPUT_DIR}/gebco_color_releif.vrt
+vrtfile2=${OUTPUT_DIR}/gebco_color_releif2.vrt
+vrtfile3=${OUTPUT_DIR}/gebco_color_releif3.vrt
+mbtilesfile=${OUTPUT_DIR}/gebco_color_releif.mbtiles
+
+[ -d "$OUTPUT_DIR" ] || mkdir -p $OUTPUT_DIR || { echo "error: $OUTPUT_DIR " 1>&2; exit 1; }
+
+echo "Builing VRT"
+gdalbuildvrt -overwrite ${vrtfile} ${INPUT_DIR}/*.tif
+echo "Builing color-relief VRT"
+gdaldem color-relief -of VRT ${vrtfile} -alpha ramp_bathymetry.ramp ${vrtfile2}
+echo "Builing gdalwarp VRT"
+gdalwarp -r cubic -s_srs epsg:4326 -t_srs EPSG:3857 ${vrtfile2} ${vrtfile3}
+echo "Import VRT into MBTiles"
+gdal_translate ${vrtfile3} ${mbtilesfile} -of MBTILES 
+#echo "Backup Origional MBTiles file"
+#cp ${mbtilesfile} ${mbtilesfile}.orig
+echo "Create MBTiles Overview"
+gdaladdo ${mbtilesfile}
+
+sqlite3 ${mbtilesfile} "UPDATE metadata SET value = 'GEBCO (2023) converted with gdaldem' WHERE name = 'description';"
+sqlite3 ${mbtilesfile} "UPDATE metadata SET value = 'baselayer' WHERE name = 'type';"
+sqlite3 ${mbtilesfile} "INSERT INTO metadata ('name','value') VALUES('attribution','GEBCO (2023)');"
